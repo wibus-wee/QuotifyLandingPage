32ce5e41e4e89d5b9c3ca15d795177ced93c899f
wibus-wee <1596355173@qq.com>
2025-02-22 18:18:54 +0800

    feat: add Umami analytics tracking script

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,19 @@ import PricingSection from "@/components/sections/PricingSection"
 import FAQSection from "@/components/sections/FAQSection"
 import UseCasesSection from "@/components/sections/UseCasesSection"
 import { usePaddle } from './lib/paddle'
+import { useEffect } from "react"
 
 export default function App() {
+  
+  useEffect(() => {
+    if (import.meta.env.PROD) {
+      const script = document.createElement('script');
+      script.src = 'https://mamiu.wibus.ren/script.js';
+      script.setAttribute('data-website-id', '34924254-664e-4c6b-a3fa-12fa4dd6942b');
+      document.head.appendChild(script);
+    }
+  }, [])
+
   const { openCheckout } = usePaddle();
   
   return (
